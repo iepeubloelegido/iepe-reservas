acc4bfe7f30e4bc177cce002dd25db336e55de0a
@victor.ortiz1 <victor.ortiz@improvein.com>
2021-06-26 20:58:39 -0300

    first commit

---
+++ docs/precache-manifest.971c51a616878c5ba084e36b15dd1a5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3f6e24dcb1b6b55cba9f2e0cb87af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3d308301a8864bfc53",
+    "url": "/static/css/2.621f69df.chunk.css"
+  },
+  {
+    "revision": "248b2a7fcefba157ae59",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "dc3d308301a8864bfc53",
+    "url": "/static/js/2.bdc6bd48.chunk.js"
+  },
+  {
+    "revision": "a2334ff9bc68c321bce67af5ebb4ec33",
+    "url": "/static/js/2.bdc6bd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248b2a7fcefba157ae59",
+    "url": "/static/js/main.73eb80c5.chunk.js"
+  },
+  {
+    "revision": "7cd28a885569a6554b28",
+    "url": "/static/js/runtime-main.939b32b0.js"
+  }
+]);
